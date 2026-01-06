fc0da04ea7a49f5b673c3108131260e8075e8c12
ivanshifman <ivanshifman1300@gmail.com>
2026-01-06 03:50:52 -0300

    chore: remove build artifacts and node_modules from repository

--- frontend/.next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__fc3cf987._.css",
-  "static/chunks/_dbbcced5._.js",
-  "static/chunks/node_modules_f2457e20._.js"
-],
-    source: "dynamic"
-});
