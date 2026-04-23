91d7ae3cdff6b4c7719a731316f4e5b79322eaf4
ivanshifman <ivanshifman1300@gmail.com>
2026-04-22 22:36:08 -0300

    remove auth folder from git cache

--- backend/src/auth/dto/get-user-id.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class GetUserIdDto {
-  @IsNotEmpty({ message: 'El email es requerido' })
-  @IsEmail({}, { message: 'El email debe ser válido' })
-  email!: string;
-}
--- backend/src/auth/dto/resend-verification.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class ResendVerificationDto {
-  @IsNotEmpty({ message: 'El ID de usuario es requerido' })
-  @IsMongoId({ message: 'El ID de usuario debe ser válido' })
-  userId!: string;
-}
